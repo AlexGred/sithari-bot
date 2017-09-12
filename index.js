768fd73f775dbcccb3fee3d489ec4786a1010ed8
Alex Dolzhenkov <alexgred,dev@gmail.com>
2017-09-12 23:42:13 +0300

    Added new commands. Added config and commands json

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
 
-const token = 'MzU2ODgwMjAzMDg1MjUwNTcx.DJhyZQ.G-3Z6ESTcC0FVzGmpvxb7cTUf0k';
+const config =  require('./config.json');
+const commands = require('./commands.json');
 
-client.on('ready', () => {
-  console.log('I am ready!');
+bot.on('ready', () => {
+  console.log('Ready!');
 });
 
-var prefix = '!';
+bot.on('message', message => {
 
-client.on('message', message => {
+  if (message.content === config.prefix + commands.help.command) {
+    let result= 'Список команд бота: \n \n';
 
-  if (message.content === prefix + 'ping') {
-    message.channel.send('pong');
+    for (var key in commands) {
+      result += config.prefix + commands[key].command + ' - ' + commands[key].description + '\n';
+    }
+
+    message.channel.send(result);
   }
+
+  if (message.content === config.prefix + commands.power.command) {
+
+  /*    if () {
+      message.channel.send(
+        {
+          file: commands.power.value
+        }
+      );      
+    }*/
+  }
+
+  if (message.content === config.prefix + commands.user.command) {
+
+    //message.channel.send(message.member.roles.keyArray());
+
+    let roleId = message.guild.roles.find('name', 'admin');
+    
+    //console.log(message.guild.roles);
+    console.log(roleId.id);
+
+    if (message.member.roles.has(roleId.id)) {
+      message.channel.send('true');
+    }
+  }
+
 });
 
-client.login(token);
+bot.login(config.token);
