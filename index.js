e361462fd7eab9d76595b340ca0717a44ece192e
Alex Dolzhenkov <alexgred,dev@gmail.com>
2017-09-13 20:55:35 +0300

    Fixed gritings command. Added rules.

--- index.js
+++ index.js
@@ -34,23 +34,13 @@ bot.on('message', message => {
     }
   }
 
-  if (message.content === config.prefix + commands.user.command) {
-
-    //message.channel.send(message.member.roles.keyArray());
-
-    let roleId = message.guild.roles.find('name', 'admin');
-    
-    //console.log(message.guild.roles);
-    console.log(roleId.id);
-
-    if (message.member.roles.has(roleId.id)) {
-      message.channel.send('true');
-    }
+  if (message.content === config.prefix + commands.rules.command) {
+    message.channel.send(commands.rules.value);
   }
 });
 
 bot.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find('name', 'member-log');
+  const channel = member.guild.channels.find('name', config.channels[0]);
 
   if (!channel) return;
   channel.send(`Привет ${member}! Я бот. Введи !help, чтобы узнать комманды.`);
