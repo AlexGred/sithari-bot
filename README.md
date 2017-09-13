bde44de11da8dece3578d10dcb12f444e5620208
Alex Dolzhenkov <alexgred.dev@gmail.com>
2017-09-13 20:04:34 +0300

    Configs and commands. Power command done.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # sithari-bot
 Bot for sithari giuld
+
+Commands
+
+{
+ <system_name_of_command>: {
+   "command": <command_name>,     
+   "description": <command_description>,
+   "value": <command_value_text_url_etc>,
+   "error": <text_for_error_or_permissin_denied>
+ }   
+} 
--- index.js
+++ index.js
@@ -22,29 +22,24 @@ bot.on('message', message => {
 
   if (message.content === config.prefix + commands.power.command) {
 
-  /*    if () {
+    if (config.specialuser.includes(message.author.username)) {
       message.channel.send(
         {
           file: commands.power.value
         }
       );      
-    }*/
-  }
-
-  if (message.content === config.prefix + commands.user.command) {
-
-    //message.channel.send(message.member.roles.keyArray());
-
-    let roleId = message.guild.roles.find('name', 'admin');
-    
-    //console.log(message.guild.roles);
-    console.log(roleId.id);
-
-    if (message.member.roles.has(roleId.id)) {
-      message.channel.send('true');
+    }
+    else {
+      message.reply(commands.power.error);
     }
   }
+});
 
+bot.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find('name', 'member-log');
+
+  if (!channel) return;
+  channel.send(`Привет ${member}! Я бот. Введи !help, чтобы узнать комманды.`);
 });
 
 bot.login(config.token);
